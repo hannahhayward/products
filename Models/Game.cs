c6a93c4ac7476181179594ace07de69c435da437
hannah hayward <hannahhayward97@gmail.com>
2021-07-20 09:25:23 -0600

    models completed

---
+++ Models/Game.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace products.Models
+{
+  public class Game
+  {
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public string ConsoleType {get; set;}
+    public string CreatorId {get;set;}
+    public DateTime CreatedAt {get;set;}
+    public DateTime UpdatedAt {get;set;}
+  }
+}
---
+++ Models/Xbox.cs
@@ -0,0 +1,7 @@
+namespace products.Models
+{
+  public class Xbox: Playstation
+  {
+    public string SpecialEdition {get;set;}
+  }
+}
